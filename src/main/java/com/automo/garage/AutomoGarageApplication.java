bc3c3ccc61f2ac7b1ea4df7f6203dd48fec66c7a
Franquelie Masanque <franquelie@gmail.com>
2025-12-25 19:44:49 +0800

    Initial commit

---
+++ src/main/java/com/automo/garage/AutomoGarageApplication.java
@@ -0,0 +1,13 @@
+package com.automo.garage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomoGarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomoGarageApplication.class, args);
+	}
+
+}
